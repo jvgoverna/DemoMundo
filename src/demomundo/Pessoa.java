4beee81e20edbf35843a4cd8f7cd4ed21553700c
jvgoverna <jv11governa2018@gmail.com>
2023-04-20 11:05:56 -0300

    Matriz e classe Pessoa
    Aumentei a matriz para 60X30 e criei a classe Pessoa

---
+++ src/demomundo/Pessoa.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package demomundo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author João Vitor
+ */
+public class Pessoa {
+    private int x,y,cor;
+    private String whatsappID;
+    private ArrayList <Pessoa> AgendaContatos = new ArrayList<Pessoa>();
+
+
+
+    public int getX() {
+        return x;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+    public int getY() {
+        return y;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+    public int getCor() {
+        return cor;
+    }
+    public void setCor(int cor) {
+        this.cor = cor;
+    }
+    public String getWhatsappID() {
+        return whatsappID;
+    }
+    public void setWhatsappID(String whatsappID) {
+        this.whatsappID = whatsappID;
+    }
+    public ArrayList<Pessoa> getAgendaContatos() {
+        return AgendaContatos;
+    }
+    public void setAgendaContatos(ArrayList<Pessoa> agendaContatos) {
+        AgendaContatos = agendaContatos;
+    }
+
+    public void addContatos(Pessoa contato){
+        for (int i = 0; i < 100; i++) {
+            AgendaContatos.add(contato);
+        }
+    }
+}
---
+++ src/demomundo/iMovable.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package demomundo;
+
+/**
+ *
+ * @author João Vitor
+ */
+public interface iMovable{
+    public void move();
+}
